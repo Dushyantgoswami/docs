1c4c7a754a468055ae354bfda1fcc774bd0f85f7
Barry Kuts <barrykuts@Barrys-MacBook-Pro.local>
2022-08-29 15:09:19 +0300

    fix navbar link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -82,7 +82,6 @@ const config = {
           {
             type: "dropdown",
             label: "Nodes",
-            to: "nodes/nodes-index",
             position: "left",
             items: [
               {
